724fa3260a3e4e415a2aad5a60e141777c83af28
fenhrir93 <vladfenhrir@gmail.com>
2023-11-25 04:49:49 +0300

    button

--- src/components/clock/box/ModeSwitcher.jsx
+++ src/components/clock/box/ModeSwitcher.jsx
@@ -1,12 +1,49 @@
 import PropTypes from "prop-types";
+import { Button } from "../../../UI/Button";
+import { useState } from "react";
+
+const BUTTONS_CONFIG = [{}];
 
 export const ModeSwitcher = ({ mode, children }) => {
+  const [clockClicked, setClockClicked] = useState(true);
+  const [timerClicked, setTimerClicked] = useState(false);
+  const [stopwatchClicked, setStopwatchClicked] = useState(false);
   return (
     <div>
-      <header className="center gap-4">
-        <button>Clock</button>
-        <button>Timer</button>
-        <button>Stopwatch</button>
+      <header className="center gap-4 h-7">
+        <Button
+          text="Clock"
+          active={clockClicked}
+          color="#7071E8"
+          activeColor="#C683D7"
+          onClick={() => {
+            setClockClicked(true);
+            setTimerClicked(false);
+            setStopwatchClicked(false);
+          }}
+        />
+        <Button
+          text="Timer"
+          active={timerClicked}
+          color="green"
+          activeColor="yellow"
+          onClick={() => {
+            setTimerClicked(true);
+            setClockClicked(false);
+            setStopwatchClicked(false);
+          }}
+        />
+        <Button
+          text="Stopwatch"
+          active={stopwatchClicked}
+          color="#7ED7C1"
+          activeColor="#F0DBAF"
+          onClick={() => {
+            setStopwatchClicked(true);
+            setClockClicked(false);
+            setTimerClicked(false);
+          }}
+        />
       </header>
       {children}
     </div>
